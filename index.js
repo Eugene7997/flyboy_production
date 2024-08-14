04c901e18544a8f31b41b3551181d127f236deca
john <ypoh007@e.ntu.edu.sg>
2024-08-14 15:08:11 +0800

    updated for production

--- index.js
+++ index.js
@@ -7,17 +7,26 @@ const cookieParser = require('cookie-parser')
 const app = express()
 const flightlogRoutes = require('./server/src/routes/flightlog.route')
 const userRoutes = require('./server/src/routes/auth.route')
+const path = require('path')
 
-app.use(cors({credentials:true, origin: "http://localhost:3000", methods: "GET,HEAD,PUT,PATCH,POST,DELETE"}))
+app.use(cors({credentials:true, origin: process.env.CLIENT_ORIGIN || "http://localhost:3000", methods: "GET,HEAD,PUT,PATCH,POST,DELETE"}))
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 app.use(cookieParser())
 app.use("/api/flightlogs", flightlogRoutes)
 app.use("/api/users", userRoutes)
 
-app.get("/", (req, res)=>{
-    res.send("App is Working");
-})
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build")); // change this if your dir structure is different
+    app.get("*", (req, res) => {
+      res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    });
+}
+else {
+    app.get("/", (req, res)=>{
+        res.send("App is Working");
+    })
+}
 
 mongoose.connect(process.env.DB_URI)
     .then(()=>{
